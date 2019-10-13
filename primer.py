feecdefe683afcf52ec5e31b16f85c3bd3045ea2
Diego Correa <diegoco66@gmail.com>
2019-10-12 21:24:05 -0500

    Pintar un cuadrado con la biblioteca turtle

---
+++ primer.py
@@ -0,0 +1,14 @@
+import turtle
+
+window = turtle.Screen()
+david = turtle.Turtle()
+david.forward(200)
+david.left(90)
+david.forward(200)
+david.left(90)
+david.forward(200)
+david.left(90)
+david.forward(200)
+david.left(90)
+
+turtle.mainloop()
