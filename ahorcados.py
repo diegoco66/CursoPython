5d2533109b2a3084ec0b64e07a3d5e267364e473
Diego Correa <diegoco66@gmail.com>
2019-11-12 21:49:49 -0500

    Ahorcados lógica

--- ahorcados.py
+++ ahorcados.py
@@ -89,6 +89,32 @@ def run():
         display_board(hidden_word, tries)
         current_letter = str(raw_input('Escoge una letra: '))
 
+        letter_indexes = []
+        for idx in range(len(word)):
+            if word[idx] == current_letter:
+                letter_indexes.append(idx)
+
+        if len(letter_indexes) == 0:
+            tries += 1
+
+            if tries == 7:
+                display_board(hidden_word, tries)
+                print('')
+                print('¡Perdiste! La palabra correcta era {}'.format(word))
+                break
+        else:
+            for idx in letter_indexes:
+                hidden_word[idx] = current_letter
+
+            letter_indexes = []
+
+        try:
+            hidden_word.index('-')
+        except ValueError:
+            print('')
+            print('Felicidades! Ganaste. La palabra es: {}'.format(word))
+            break
+
 def random_word():
     idx = random.randint(0, len(WORDS) - 1)
     return WORDS[idx]
