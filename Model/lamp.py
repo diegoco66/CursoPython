bce4a671c7fde16d97884bca09caa75b79d65894
Diego Correa <diegoco66@gmail.com>
2019-11-26 21:58:40 -0500

    Clase

---
+++ Model/lamp.py
@@ -0,0 +1,56 @@
+
+class Lamp:
+    _LAMPS = ['''
+            |   
+         \ .-. /        
+       -- (   ) --
+           \ /
+          _|=|_
+         |_____|  
+    ''', '''
+
+           .-. 
+          (   )
+           \ /
+          _|=|_
+         |_____|  
+    ''']
+
+    def __init__(self, state):
+        self._is_turned_on = state
+
+    def turn_on(self):
+        self._is_turned_on = True
+        self._display_image()
+
+    def turn_off(self):
+        self._is_turned_on = False
+        self._display_image()
+
+    def _display_image(self):
+        if self._is_turned_on:
+            print(self._LAMPS[0])
+        else:
+            print(self._LAMPS[1])
+
+def run():
+    lamp = Lamp(False)
+
+    while True:
+        command = str(input('''
+        ¿Qué deseas hacer?
+        
+        [p]render
+        [a]pagar
+        [s]alir
+        '''))
+
+        if command == 'p':
+            lamp.turn_on()
+        elif command == 'a':
+            lamp.turn_off()
+        else:
+            break
+
+if __name__ == '__main__':
+    run()
